b673534574658709142182db25d45fd51a1c6e7b
lijinggai <2786810048@qq.com>
2022-10-04 15:33:13 +0800

    test.cpp_10_03/

---
+++ test.cpp_10_03/test.cpp_10_03/test.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+int sum(int x ,int y)
+{
+	if (x == y)
+	{
+		return -1;
+	}
+	if (x > y)
+		return x;
+	else
+		return y;
+}
+int main()
+{
+	int a, b;
+	cin >> a;
+	cin >> b;
+	int max=sum(a,b);
+	if(max==-1)
+		cout << "相等" << endl;
+
+	else
+		cout << max;
+}
